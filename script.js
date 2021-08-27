62d76fc2a4200a220b1a07e03b127b6db6410af8
Braydon Wang <braydon.wang@gmail.com>
2021-08-26 23:38:34 -0400

    Added more animations

--- script.js
+++ script.js
@@ -16,3 +16,35 @@ window.addEventListener('scroll', function() {
   let value = Math.min(0.8,1 + window.scrollY/-600);
   video.style.opacity = value;
 })
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 50;
+
+    if (revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', skillbar);
+
+function skillbar() {
+  var reveals = document.querySelectorAll('.skillbar');
+
+  for (var i = 0; i < reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 50;
+
+    if (revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+  }
+}
